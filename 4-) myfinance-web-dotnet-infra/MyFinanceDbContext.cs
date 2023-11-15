4978ad7841fe38e1b918c75528380e03e54b70f8
Diego Veiga <diegogv.eng@gmail.com>
2023-11-15 00:21:29 -0300

    Aula: Tela de Listagem de Plano de Contas

--- 4-) myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ 4-) myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -12,9 +12,7 @@ public class MyFinanceDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;
-"); 
-        // Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;
+        optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;TrustServerCertificate=True;"); 
 
         base.OnConfiguring(optionsBuilder);
     }
