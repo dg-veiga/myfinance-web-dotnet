aa1df551192dc22e7bce09dab5bcb0e35ed1d836
Diego Veiga <diegogv.eng@gmail.com>
2023-11-18 23:19:48 -0300

    Aula: Refactor 2 - Classe Genérica para Repositórios (Parte 1)

---
+++ 4-) myfinance-web-dotnet-infra/Interfaces/Base/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using myfinance_web_dotnet_domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myfinance_web_dotnet_infra.Interfaces.Base
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Cadastrar(TEntity Entidade);
+        void Excluir(int Id);
+        List<TEntity> ListarRegistros();
+        TEntity RetornarRegistro(int Id);
+    }
+}
---
+++ 4-) myfinance-web-dotnet-infra/Repository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using myfinance_web_dotnet_infra.Interfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myfinance_web_dotnet_infra
+{
+    public abstract class Repository<TEntity> : MyFinanceDbContext, IRepository<TEntity> where TEntity : class
+    {
+        protected Repository(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public void Cadastrar(TEntity Entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> ListarRegistros()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity RetornarRegistro(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
