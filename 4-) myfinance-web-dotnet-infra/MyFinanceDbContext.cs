3574eaa54647ddc3e7171df299e97d73b30b4901
Diego Veiga <diegogv.eng@gmail.com>
2023-11-18 22:44:29 -0300

    Aula: Refactor 1 - Utilizando Variáveis de Ambiente

--- 4-) myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ 4-) myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -1,19 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using myfinance_web_dotnet_domain.Entities;
 
 namespace myfinance_web_dotnet_infra;
 
 public class MyFinanceDbContext : DbContext
 {
+    private readonly IConfiguration _configuration;
+
     public DbSet<PlanoConta> PlanoConta {get; set;}
 
     public DbSet<Transacao> Transacao {get; set;}
 
+    public MyFinanceDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;TrustServerCertificate=True;"); 
+        var connectionString = _configuration.GetConnectionString("Database");
 
-        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer(connectionString);
     }
 }
