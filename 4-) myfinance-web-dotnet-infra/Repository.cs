c99e8c2d91f20dfc2a0bef9238ae8c020548aa03
Diego Veiga <diegogv.eng@gmail.com>
2023-11-19 01:18:15 -0300

    Aula: Refactor 2 - Classe Genérica para Repositórios (Parte 2)

--- 4-) myfinance-web-dotnet-infra/Repository.cs
+++ 4-) myfinance-web-dotnet-infra/Repository.cs
@@ -1,37 +1,50 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using myfinance_web_dotnet_domain.Entities.Base;
 using myfinance_web_dotnet_infra.Interfaces.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace myfinance_web_dotnet_infra
 {
-    public abstract class Repository<TEntity> : MyFinanceDbContext, IRepository<TEntity> where TEntity : class
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : EntityBase, new()
     {
-        protected Repository(IConfiguration configuration) : base(configuration)
+        protected DbContext Db;
+        protected DbSet<TEntity> DbSetContext;
+
+        protected Repository(DbContext dbContext)
         {
+            Db = dbContext;
+            DbSetContext = Db.Set<TEntity>();
         }
 
         public void Cadastrar(TEntity Entidade)
         {
-            throw new NotImplementedException();
+            if (Entidade.Id == null)
+            {
+                DbSetContext.Add(Entidade);
+            }
+            else
+            {
+                DbSetContext.Attach(Entidade);
+                Db.Entry(Entidade).State = EntityState.Modified;
+            }
+            Db.SaveChanges();
         }
 
         public void Excluir(int Id)
         {
-            throw new NotImplementedException();
+            var entidade = new TEntity() { Id = Id };
+            Db.Attach(entidade);
+            Db.Remove(entidade);
+            Db.SaveChanges();
         }
 
         public List<TEntity> ListarRegistros()
         {
-            throw new NotImplementedException();
+            return DbSetContext.ToList();
         }
 
         public TEntity RetornarRegistro(int Id)
         {
-            throw new NotImplementedException();
+            return DbSetContext.Where(x => x.Id == Id).First();
         }
     }
 }
